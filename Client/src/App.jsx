b8e622965bb15aafbd334648d4af13a934fda991
Devansh <purohitdevansh@gmail.com>
2025-06-11 12:41:19 +0530

    new changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './Pages/HomePage'
+import Login from './Pages/Login'
 
 const App = () => {
   return (
    <BrowserRouter>
    <Routes>
     <Route path='/' element={<HomePage/>}  />
+     <Route path='/login' element={<Login/>}  />
    </Routes>
    </BrowserRouter>
   )
